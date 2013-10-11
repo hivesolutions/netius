eb58118047d296dbf5dbd65a930602ade0a792cf
joamag <joamag@gmail.com>
2013-10-11 19:02:38 +0100

    initial commit

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Hive Pingu System
+# Hive Netium System
 # Copyright (C) 2008-2012 Hive Solutions Lda.
 #
-# This file is part of Hive Pingu System.
+# This file is part of Hive Netium System.
 #
-# Hive Pingu System is free software: you can redistribute it and/or modify
+# Hive Netium System is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Hive Pingu System is distributed in the hope that it will be useful,
+# Hive Netium System is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Hive Pingu System. If not, see <http://www.gnu.org/licenses/>.
+# along with Hive Netium System. If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
