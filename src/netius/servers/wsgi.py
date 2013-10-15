51a5f3c51e36a32bec7baf4fe299798c1752d37b
joamag <joamag@gmail.com>
2013-10-15 12:23:06 +0100

    new query string and info support

--- src/netius/servers/wsgi.py
+++ src/netius/servers/wsgi.py
@@ -43,7 +43,7 @@ import http
 
 class WSGIServer(http.HTTPServer):
 
-    def __init__(self, app, name = None, handler = None, *args, **kwargs):
+    def __init__(self, app, mount = "", name = None, handler = None, *args, **kwargs):
         http.HTTPServer.__init__(
             self,
             name = name,
@@ -52,27 +52,49 @@ class WSGIServer(http.HTTPServer):
             **kwargs
         )
         self.app = app
+        self.mount = mount
+        self.mount_l = len(mount)
 
     def on_data_http(self, connection, parser):
         http.HTTPServer.on_data_http(self, connection, parser)
 
+        # clojure method to be used to close the current connection in
+        # case that's required by the current connection headers
         def close(data):
             self.on_connection_d(connection)
 
+        # method created as a clojure that handles the starting of
+        # response as defined in the wsgi standards
         def start_response(status, headers):
             return self._start_response(connection, status, headers)
 
+        # retrieves the path for the current request and then retrieves
+        # the query string part for it also, after that computes the
+        # path info value as the substring of the path without the mount
+        path = parser.get_path()
+        query = parser.get_query()
+        path_info = path[self.mount_l:]
+
+        # initializes the environment map (structure) with all the cgi based
+        # variables that should enable the application to handle the request
+        # and respond to it in accordance
         environ = dict(
             REQUEST_METHOD = parser.method.upper(),
-            SCRIPT_NAME = "",
-            PATH_INFO = parser.path_s,
-            QUERY_STRING = "",
+            SCRIPT_NAME = self.mount,
+            PATH_INFO = path_info,
+            QUERY_STRING = query,
             CONTENT_TYPE = parser.headers.get("content-type", None),
             CONTENT_LENGTH = None if parser.content_l == -1 else parser.content_l,
+            SERVER_NAME = http.SERVER_NAME,
+            SERVER_PORT = self.port,
+            SERVER_PROTOCOL = parser.version_s
         )
 
+        # updates the environment map with all the structures referring
+        # to the wsgi specifications note that the message is retrieved
+        # as a buffer to be able to handle the file specific operations
         environ["wsgi.version"] = (1, 0)
-        environ["wsgi.url_scheme"] = "http"
+        environ["wsgi.url_scheme"] = "https" if connection.ssl else "http"
         environ["wsgi.input"] = parser.get_message_b()
         environ["wsgi.errors"] = sys.stderr
         environ["wsgi.multithread"] = True
@@ -87,6 +109,9 @@ class WSGIServer(http.HTTPServer):
             key = "HTTP_" + key.upper()
             environ[key] = value
 
+        # runs the app logic with the provided environment map and start
+        # response clojure and then iterates over the complete set of values
+        # in the returned iterator to send the messages to the other end
         sequence = self.app(environ, start_response)
         for value in sequence: connection.send(value)
 
@@ -122,6 +147,15 @@ def application(environ, start_response):
     start_response("200 OK", headers)
     yield message
 
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def hello():
+    return "Hello World!"
+
 if __name__ == "__main__":
-    server = WSGIServer(application)
-    server.serve()
+    server = WSGIServer(app, mount = "/hello")
+    server.serve(host = "0.0.0.0")
