6907c4433196f70d4027b8031d7d998c556780c7
joamag <joamag@gmail.com>
2013-10-23 23:22:47 +0100

    new socks support

---
+++ src/netius/servers/socks.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Netius System
+# Copyright (C) 2008-2012 Hive Solutions Lda.
+#
+# This file is part of Hive Netius System.
+#
+# Hive Netius System is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Netius System is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Netius System. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import http
+
+import netius.clients
+
+class SocksServer(netius.Server):
+
+    def __init__(self, rules = {}, name = None, handler = None, *args, **kwargs):
+        netius.Server.__init__(
+            self,
+            name = name,
+            handler = handler,
+            *args,
+            **kwargs
+        )
+        self.rules = rules
+        self.conn_map = {}
+
+        self.raw_client = netius.clients.RawClient(
+            thread = False,
+            *args,
+            **kwargs
+        )
+        self.raw_client.bind("connect", self._on_raw_connect)
+        self.raw_client.bind("data", self._on_raw_data)
+        self.raw_client.bind("close", self._on_raw_close)
+
+        self.container = netius.Container(*args, **kwargs)
+        self.container.add_base(self)
+        self.container.add_base(self.raw_client)
+
+    def start(self):
+        self.raw_client.load()
+        self.container.start()
+
+    def stop(self):
+        self.container.stop()
+
+    def on_data(self, connection, data):
+        netius.Server.on_data(self, connection, data)
+
+        if hasattr(connection, "tunnel_c"): connection.tunnel_c.send(data)
+
+    def on_connection_d(self, connection):
+        netius.Server.on_connection_d(self, connection)
+
+        if hasattr(connection, "tunnel_c"): connection.tunnel_c.close()
+
+    def on_data_http(self, connection, parser):
+        http.HTTPServer.on_data_http(self, connection, parser)
+
+        method = parser.method.upper()
+        path = parser.path_s
+        version_s = parser.version_s
+
+        import re
+        rule = re.compile(".*facebook.com.*")
+        rejected = rule.match(path)
+
+        if rejected:
+            connection.send(
+                "%s 403 Forbidden\r\n\r\nThis connection is not allowed" % version_s,
+                callback = self._prx_close
+            )
+            return
+
+        if method == "CONNECT":
+            host, port = path.split(":")
+            port = int(port)
+            _connection = self.raw_client.connect(host, port)
+            connection.tunnel_c = _connection
+            self.conn_map[_connection] = connection
+        else:
+            _connection = self.http_client.method(
+                method,
+                path,
+                headers = parser.headers
+            )
+            _connection.used = False
+            connection.proxy_c = _connection
+            self.conn_map[_connection] = connection
+
+    def _on_raw_connect(self, client, _connection):
+        connection = self.conn_map[_connection]
+        connection.send_response(
+            code = 200,
+            code_s = "Connection established"
+        )
+
+    def _on_raw_data(self, client, _connection, data):
+        connection = self.conn_map[_connection]
+        connection.send(data)
+
+    def _on_raw_close(self, client, _connection):
+        connection = self.conn_map[_connection]
+        connection.close(flush = True)
+        del self.conn_map[_connection]
+
+if __name__ == "__main__":
+    import logging
+    server = SocksServer(level = logging.INFO)
+    server.serve(host = "0.0.0.0", port = 8080)
