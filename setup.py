f498c648b3086a08f60b52fe6ed08706c14411dc
joamag <joamag@gmail.com>
2013-12-05 12:07:18 +0000

    new binaries and version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ import setuptools
 
 setuptools.setup(
     name = "netius",
-    version = "0.2.2",
+    version = "0.2.3",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
