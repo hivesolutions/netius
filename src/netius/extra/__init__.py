7d20c263e101d93b0def665153a5bacc402b1353
joamag <joamag@gmail.com>
2013-12-06 18:30:53 +0000

    small init code refactor

--- src/netius/extra/__init__.py
+++ src/netius/extra/__init__.py
@@ -36,6 +36,10 @@ __license__ = "GNU General Public License (GPL), Version 3"
 
 import desktop
 import dhcp_s
+import proxy_f
+import proxy_r
 
 from desktop import *
 from dhcp_s import *
+from proxy_f import *
+from proxy_r import *
