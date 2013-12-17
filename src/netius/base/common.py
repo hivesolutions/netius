710ab4da932db4a233bc2ac248ad5a02c37ecdf3
joamag <joamag@gmail.com>
2013-12-17 23:55:42 +0000

    iterates over all the connections

--- src/netius/base/common.py
+++ src/netius/base/common.py
@@ -397,6 +397,9 @@ class Base(observer.Observable):
             # of connection for each of the three operations returning
             # the resulting active sets for the callbacks
             reads, writes, errors = self.poll.poll()
+            
+            for connection in self.connections:
+                print connection.pending
 
             # calls the various callbacks with the selections lists,
             # these are the main entry points for the logic to be executed
