8f59690fa6382f91c8a1506577a13f4c8444ae9d
joamag <joamag@gmail.com>
2013-12-18 00:28:29 +0000

    new valid errors

--- src/netius/base/server.py
+++ src/netius/base/server.py
@@ -480,7 +480,7 @@ class StreamServer(Server):
                 connection.close()
         except socket.error, error:
             error_v = error.args[0]
-            if error_v in SSL_VALID_ERRORS:
+            if error_v in VALID_ERRORS:
                 self.wready = False
             elif error_v in SILENT_ERRORS:
                 connection.close()
