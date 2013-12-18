a0111c49e9a4930ba03382484602fba2aa01c646
joamag <joamag@gmail.com>
2013-12-18 02:00:55 +0000

    reverted callbacks

--- src/netius/base/conn.py
+++ src/netius/base/conn.py
@@ -290,11 +290,6 @@ class Connection(object):
         return self.status == PENDING
 
     def _send(self):
-        # creates the list that is going to be used to store the
-        # complete set of callbacks to be called at the end of the
-        # send operation (deferred callback call)
-        callbacks = []
-        
         # sets the write ready flag so that any further request to
         # write operation will be immediately performed
         self.wready = True
@@ -349,7 +344,7 @@ class Connection(object):
                     # sent latter (only then the callback is called)
                     is_valid = count == data_l
                     if is_valid:
-                        if callback: callbacks.append(callback)
+                        callback and callback(self)
                     else:
                         data_o = (data[count:], callback)
                         self.pending.append(data_o)
@@ -358,10 +353,6 @@ class Connection(object):
             # exists and no access to the pending is prevented
             self.pending_lock.release()
             
-            # iterates over all the callbacks that are meant to
-            # be called in order for them to be processed
-            for callback in callbacks: callback(self)
-
         # removes the current connection from the set of connection
         # that are monitored for any write event (no longer required)
         self.remove_write()
