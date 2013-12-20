d01069cdb3229c220cddef75ca3bab46bd419db5
joamag <joamag@gmail.com>
2013-12-20 11:46:04 +0000

    fixed small bug

--- src/netius/servers/socks.py
+++ src/netius/servers/socks.py
@@ -185,10 +185,11 @@ class SOCKSServer(netius.StreamServer):
         )
 
     def _throttle(self, _connection):
-        if not _connection.renable == False: return
         if _connection.pending_s > self.min_pending: return
 
         connection = self.conn_map[_connection]
+        if not _connection.renable == False: return
+
         connection.enable_read()
         self.reads((connection.socket,), state = False)
 
