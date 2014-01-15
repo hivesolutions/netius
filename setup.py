e393ddc14276fca5fd14df384304a57ccee01dca
joamag <joamag@gmail.com>
2014-01-15 12:00:31 +0000

    small code fix

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ import setuptools
 
 setuptools.setup(
     name = "netius",
-    version = "0.8.21",
+    version = "0.8.22",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
