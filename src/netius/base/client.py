76fcab6a74ae3249681ba5692fc7576ae3c2a009
joamag <joamag@gmail.com>
2014-01-21 18:12:59 +0000

    initial versionof the datagram client infra-structure

--- src/netius/base/client.py
+++ src/netius/base/client.py
@@ -84,37 +84,203 @@ class Client(Base):
             self.on_error(error)
 
 class DatagramClient(Client):
-    pass
 
 
+    # @todo tenho sempre de ter um socket registardo no gajo
+    # como manda a lei
 
+    def __init__(self, *args, **kwargs):
+        Client.__init__(self, *args, **kwargs)
+        self.renable = True
+        self.wready = True
+        self.pending_s = 0
+        self.pending = []
+        self.pending_lock = threading.RLock()
 
-    def send_to(self, host, port):
-        if not host: raise errors.NetiusError("Invalid host for connect operation")
-        if not port: raise errors.NetiusError("Invalid port for connect operation")
+    def on_read(self, _socket):
+        try:
+            # verifies if there's any pending operations in the
+            # socket (eg: ssl handshaking) and performs them trying
+            # to finish them, in they are still pending at the current
+            # state returns immediately (waits for next loop)
+            if self._pending(_socket): return
 
-        _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        _socket.setblocking(0)
+            # iterates continuously trying to read as much data as possible
+            # when there's a failure to read more data it should raise an
+            # exception that should be handled properly
+            while True:
+                data, address = _socket.recvfrom(CHUNK_SIZE)
+                self.on_data(address, data)
+        except ssl.SSLError, error:
+            error_v = error.args[0]
+            if error_v in SSL_SILENT_ERRORS:
+                self.debug(error)
+            elif not error_v in SSL_VALID_ERRORS:
+                self.warning(error)
+                lines = traceback.format_exc().splitlines()
+                for line in lines: self.info(line)
+        except socket.error, error:
+            error_v = error.args[0]
+            if error_v in SILENT_ERRORS:
+                self.debug(error)
+            elif not error_v in VALID_ERRORS:
+                self.warning(error)
+                lines = traceback.format_exc().splitlines()
+                for line in lines: self.info(line)
+        except BaseException, exception:
+            self.warning(exception)
+            lines = traceback.format_exc().splitlines()
+            for line in lines: self.info(line)
 
-        _socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        _socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        _socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-        if self.receive_buffer: _socket.setsockopt(
-            socket.SOL_SOCKET,
-            socket.SO_RCVBUF,
-            self.receive_buffer
-        )
-        if self.send_buffer: _socket.setsockopt(
-            socket.SOL_SOCKET,
-            socket.SO_SNDBUF,
-            self.send_buffer
-        )
-        self._socket_keepalive(_socket)
+    def on_write(self, _socket):
+        try:
+            self._send(_socket)
+        except ssl.SSLError, error:
+            error_v = error.args[0]
+            if error_v in SSL_SILENT_ERRORS:
+                self.debug(error)
+            elif not error_v in SSL_VALID_ERRORS:
+                self.warning(error)
+                lines = traceback.format_exc().splitlines()
+                for line in lines: self.info(line)
+        except socket.error, error:
+            error_v = error.args[0]
+            if error_v in SILENT_ERRORS:
+                self.debug(error)
+            elif not error_v in VALID_ERRORS:
+                self.warning(error)
+                lines = traceback.format_exc().splitlines()
+                for line in lines: self.info(line)
+        except BaseException, exception:
+            self.warning(exception)
+            lines = traceback.format_exc().splitlines()
+            for line in lines: self.info(line)
 
-        self.sub_all(_socket)
+    def on_error(self, _socket):
+        pass
 
-        return _socket
+    def on_data(self, connection, data):
+        pass
 
+    def ensure_write(self):
+        # retrieves the identifier of the current thread and
+        # checks if it's the same as the one defined in the
+        # owner in case it's not then the operation is not
+        # considered to be safe and must be delayed
+        tid = thread.get_ident()
+        is_safe = tid == self.tid
+
+        # in case the thread where this code is being executed
+        # is not the same the operation is considered to be not
+        # safe and so it must be delayed to be executed in the
+        # next loop of the thread cycle, must return immediately
+        # to avoid extra subscription operations
+        if not is_safe: self.delay(self.ensure_write); return
+
+        # adds the current socket to the list of write operations
+        # so that it's going to be available for writing as soon
+        # as possible from the poll mechanism
+        self.sub_write(self.socket)
+
+    def remove_write(self):
+        self.unsub_write(self.socket)
+
+    def enable_read(self):
+        if not self.renable == False: return
+        self.renable = True
+        self.sub_read(self.socket)
+
+    def disable_read(self):
+        if not self.renable == True: return
+        self.renable = False
+        self.unsub_read(self.socket)
+
+    def send(self, data, address, delay = False, callback = None):
+        data_l = len(data)
+
+        if callback: data = (data, callback)
+        data = (data, address)
+
+        tid = thread.get_ident()
+        is_safe = tid == self.tid
+
+        self.pending_lock.acquire()
+        try: self.pending.insert(0, data)
+        finally: self.pending_lock.release()
+
+        self.pending_s += data_l
+
+        if self.wready:
+            if is_safe and not delay: self._flush_write()
+            else: self.delay(self._flush_write, verify = True)
+        else:
+            self.ensure_write()
+
+    def _send(self, _socket):
+        self.wready = True
+        self.pending_lock.acquire()
+        try:
+            while True:
+                if not self.pending: break
+                data = self.pending.pop()
+                data_o = data
+                callback = None
+                data, address = data
+                if type(data) == types.TupleType:
+                    data, callback = data
+                data_l = len(data)
+
+                try:
+                    # tries to send the data through the socket and
+                    # retrieves the number of bytes that were correctly
+                    # sent through the socket, this number may not be
+                    # the same as the size of the data in case only
+                    # part of the data has been sent
+                    count = _socket.sendto(data, address)
+                except:
+                    # sets the current connection write ready flag to false
+                    # so that a new level notification must be received
+                    self.wready = False
+
+                    # ensures that the write event is going to be triggered
+                    # this is required for so that the remaining pending
+                    # data is going to be correctly written
+                    self.ensure_write()
+
+                    # in case there's an exception must add the data
+                    # object to the list of pending data because the data
+                    # has not been correctly sent
+                    self.pending.append(data_o)
+                    raise
+                else:
+                    # decrements the size of the pending buffer by the number
+                    # of bytes that were correctly send through the buffer
+                    self.pending_s -= count
+
+                    # verifies if the data has been correctly sent through
+                    # the socket and for suck situations calls the callback
+                    # object, otherwise creates a new data object with only
+                    # the remaining (partial data) and the callback to be
+                    # sent latter (only then the callback is called)
+                    is_valid = count == data_l
+                    if is_valid:
+                        callback and callback(self)
+                    else:
+                        data_o = ((data[count:], callback), address)
+                        self.pending.append(data_o)
+        finally:
+            self.pending_lock.release()
+
+        self.remove_write()
+
+    def _flush_write(self):
+        """
+        Flush operations to be called by the delaying controller
+        (in ticks) that will trigger all the write operations
+        pending for the current connection's socket.
+        """
+
+        self.writes((self.socket,), state = False)
 
 class StreamClient(Client):
 
