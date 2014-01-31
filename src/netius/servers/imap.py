a467eddea73c50e211b6f2f65412615fd776f342
joamag <joamag@gmail.com>
2014-01-31 16:49:36 +0000

    new server values

--- src/netius/servers/imap.py
+++ src/netius/servers/imap.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Hive Netius System. If not, see <http://www.gnu.org/licenses/>.
 
+__author__ = "João Magalhães joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
@@ -33,27 +36,3 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
-
-import dhcp
-import echo
-import http
-import imap
-import mjpg
-import pop
-import proxy
-import smtp
-import socks
-import ws
-import wsgi
-
-from dhcp import *
-from echo import *
-from http import *
-from imap import *
-from mjpg import *
-from pop import *
-from proxy import *
-from smtp import *
-from socks import *
-from ws import *
-from wsgi import *
