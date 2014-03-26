f62bdb2a8950ba73f62e94b0b32646f4a1f4b4f7
joamag <joamag@gmail.com>
2014-03-26 23:22:56 +0000

    initial dkim structure

--- src/netius/common/dkim.py
+++ src/netius/common/dkim.py
@@ -36,3 +36,38 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
+
+import re
+
+def rfc822_parse(message, exclude = ()):
+    """
+    Parse a message in RFC822 format.
+
+    @param message: The message in RFC822 format. Either CRLF or LF is an accepted line separator.
+    @return Returns a tuple of (headers, body) where headers is a list of (name, value) pairs.
+    The body is a CRLF-separated string.
+    """
+
+    headers = []
+    lines = re.split("\r?\n", message)
+    i = 0
+    while i < len(lines):
+        if len(lines[i]) == 0:
+            # End of headers, return what we have plus the body, excluding the blank line.
+            i += 1
+            break
+        if re.match(r"[\x09\x20]", lines[i][0]):
+            headers[-1][1] += lines[i]+"\r\n"
+        else:
+            m = re.match(r"([\x21-\x7e]+?):", lines[i])
+            if m is not None:
+                headers.append([m.group(1), lines[i][m.end(0):]+"\r\n"])
+            elif lines[i].startswith("From "):
+                pass
+            else:
+                raise MessageFormatError("Unexpected characters in RFC822 header: %s" % lines[i])
+        i += 1
+    return (headers, "\r\n".join(lines[i:]))
+
+def dkim_sign(message, selector, domain, private_key):
+    headers, body = rfc822_parse(message)
