335e9f5f4da8494c3db145e492d69988573393ba
joamag <joamag@gmail.com>
2014-03-27 17:31:29 +0000

    new init

--- src/netius/sh/__init__.py
+++ src/netius/sh/__init__.py
@@ -34,8 +34,10 @@ __copyright__ = "Copyright (c) 2008-2012 Hive Solutions Lda."
 __license__ = "GNU General Public License (GPL), Version 3"
 """ The license for the module """
 
+import base
 import dkim
 import rsa
 
+from base import *
 from dkim import *
 from rsa import *
