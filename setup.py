e377606060ba462e3a94ecefb9528eeda59ff43c
joamag <joamag@gmail.com>
2014-04-21 14:26:30 +0100

    new python 3.4 compatibility

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.0",
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3"
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4"
     ]
 )
