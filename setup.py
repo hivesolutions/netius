950a88180b7c06f46b195ae999fec0b67ed9acc8
joamag <joamag@gmail.com>
2014-06-19 20:00:02 +0100

    new version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ import setuptools
 
 setuptools.setup(
     name = "netius",
-    version = "1.2.7",
+    version = "1.2.8",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
