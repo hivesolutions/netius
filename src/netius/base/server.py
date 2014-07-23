3ee123d67bbacfdc2452a1482e724b2e6a1dae30
joamag <joamag@gmail.com>
2014-07-23 14:03:27 +0100

    new logging loading

--- src/netius/base/server.py
+++ src/netius/base/server.py
@@ -125,6 +125,7 @@ class Server(Base):
         # account if the environment variable is currently set or not
         # please note that some side effects may arise from this set
         if env: self.level = self.get_env("LEVEL", self.level)
+        if env: self.logging = self.get_env("LOGGING", self.logging)
         if env: self.poll_name = self.get_env("POLL", self.poll_name)
         if env: self.poll_timeout = self.get_env(
             "POLL_TIMEOUT",
