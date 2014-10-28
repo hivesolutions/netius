861d95cbeef0c25ffc6da546abf3b6fd7c379c56
joamag <joamag@gmail.com>
2014-10-28 19:10:14 +0000

    new async file server

---
+++ src/netius/extra/filea.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Netius System
+# Copyright (C) 2008-2014 Hive Solutions Lda.
+#
+# This file is part of Hive Netius System.
+#
+# Hive Netius System is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Netius System is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Netius System. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2014 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+from netius.extra import file as _file
+
+class FileAsyncServer(_file.FileServer):
+
+    def _file_send(self, connection):
+        file = connection.file
+        range = connection.range
+        is_larger = _file.BUFFER_SIZE > connection.bytes_p
+        buffer_s = connection.bytes_p if is_larger else _file.BUFFER_SIZE
+
+        def callback(data, *args, **kwargs):
+            data_l = len(data) if data else 0
+            connection.bytes_p -= data_l
+            is_final = not data or connection.bytes_p == 0
+            callback = self._file_finish if is_final else self._file_send
+            connection.send(
+                data,
+                delay = True,
+                callback = callback
+            )
+
+        self.fread(file, buffer_s, data = callback)
+
+if __name__ == "__main__":
+    import logging
+
+    server = FileAsyncServer(level = logging.INFO)
+    server.serve(env = True)
