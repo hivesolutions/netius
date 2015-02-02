4d5432e1fcaf94715cb4975b539be8197a6c30b4
joamag <joamag@gmail.com>
2015-02-02 22:19:26 +0000

    new version of netius (changed readme.rst)

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ import setuptools
 
 setuptools.setup(
     name = "netius",
-    version = "1.5.1",
+    version = "1.5.2",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
