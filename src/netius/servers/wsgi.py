e02eb361b0862416214582fd4166ea63c6d575ba
joamag <joamag@gmail.com>
2015-03-31 18:24:46 +0100

    reverted header conversion for python 3 (not required)

--- src/netius/servers/wsgi.py
+++ src/netius/servers/wsgi.py
@@ -235,7 +235,6 @@ class WSGIServer(http.HTTPServer):
         buffer = []
         buffer.append("%s %s\r\n" % (version_s, status))
         for key, value in headers.items():
-            value = netius.legacy.str(value)
             buffer.append("%s: %s\r\n" % (key, value))
         buffer.append("\r\n")
 
