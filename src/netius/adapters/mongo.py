857192ee541ac02cae33c1deb100a1863a49b9bc
joamag <joamag@gmail.com>
2015-08-05 09:39:32 +0100

    new name in header

--- src/netius/adapters/mongo.py
+++ src/netius/adapters/mongo.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the Apache License along with
 # Hive Netius System. If not, see <http://www.apache.org/licenses/>.
 
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
