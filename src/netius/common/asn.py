fe395abc7a426bf6e132e25a1f07217cbb1ac1a4
joamag <joamag@gmail.com>
2015-08-05 15:47:46 +0100

    new copright information

--- src/netius/common/asn.py
+++ src/netius/common/asn.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Hive Netius System
-# Copyright (C) 2008-2015 Hive Solutions Lda.
+# Copyright (c) 2008-2015 Hive Solutions Lda.
 #
 # This file is part of Hive Netius System.
 #
