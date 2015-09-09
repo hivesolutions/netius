b0893992065eedd95edeac825ce1dbbc16b786b3
joamag <joamag@gmail.com>
2015-09-09 17:54:45 +0100

    new busy connections values

--- src/netius/extra/proxy_r.py
+++ src/netius/extra/proxy_r.py
@@ -83,7 +83,7 @@ class ReverseProxyServer(netius.servers.ProxyServer):
             robin = dict(),
             smart = netius.common.PriorityDict()
         )
-        self.busy_c = 0
+        self.busy_conn = 0
         self.balancer_m = getattr(self, "balancer_" + self.strategy)
         self.acquirer_m = getattr(self, "acquirer_" + self.strategy)
         self.releaser_m = getattr(self, "releaser_" + self.strategy)
@@ -93,7 +93,7 @@ class ReverseProxyServer(netius.servers.ProxyServer):
         info.update(
             reuse = self.reuse,
             strategy = self.strategy,
-            busy_c = self.busy_c
+            busy_conn = self.busy_conn
         )
         return info
 
@@ -321,7 +321,7 @@ class ReverseProxyServer(netius.servers.ProxyServer):
         return prefix, (prefix, queue)
 
     def acquirer(self, state):
-        self.busy_c += 1
+        self.busy_conn += 1
         self.acquirer_m(state)
 
     def acquirer_robin(self, state):
@@ -335,7 +335,7 @@ class ReverseProxyServer(netius.servers.ProxyServer):
         queue[prefix] = sorter
 
     def releaser(self, state):
-        self.busy_c -= 1
+        self.busy_conn -= 1
         self.releaser_m(state)
 
     def releaser_robin(self, state):
