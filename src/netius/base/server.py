ac9cade1517d41c2020d1d88d059459fd01214fe
joamag <joamag@gmail.com>
2015-09-09 09:19:19 +0100

    new code

--- src/netius/base/server.py
+++ src/netius/base/server.py
@@ -674,8 +674,8 @@ class StreamServer(Server):
         # verifies a series of pre-conditions on the socket so
         # that it's ensured to be in a valid state before it's
         # set as a new connection for the server (validation)
+        if not self.ssl and socket_c._closed: socket_c.close(); return
         if self.ssl and not socket_c._sslobj: socket_c.close(); return
-        elif socket_c._closed: socket_c.close(); return
 
         # in case the ssl mode is enabled, "patches" the socket
         # object with an extra pending reference, that is going
