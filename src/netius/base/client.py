7dbea725aec4727bf84c670f7fbf88efa362d3fa
joamag <joamag@gmail.com>
2015-10-27 16:30:21 +0000

    new connection information

--- src/netius/base/client.py
+++ src/netius/base/client.py
@@ -607,6 +607,9 @@ class StreamClient(Client):
         # no connection available or the status is not open
         # must return the control flow immediately to the caller
         connection = self.connections_m.get(_socket, None)
+        print("connection := %s\n" % str(connection.id))
+        import sys
+        sys.stdout.flush()
         if not connection: return
         if not connection.status == OPEN: print("not open\n"); return
         if not connection.renable == True: print("not read enable\n"); return
