00ada2fc4cd092aa89a6dcbb9fb1887a06c46fc1
joamag <joamag@hive.pt>
2016-03-10 11:07:45 +0000

    new relative import

--- src/netius/sh/smtp.py
+++ src/netius/sh/smtp.py
@@ -37,7 +37,7 @@ __copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
 __license__ = "Apache License, Version 2.0"
 """ The license for the module """
 
-import base
+from . import base
 
 import netius.clients
 
