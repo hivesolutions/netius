afa0f115ceaff3c9c6fb5dd67e0d0fb91b6f47d3
joamag <joamag@gmail.com>
2016-04-17 12:15:52 +0100

    new python version added to setup.py

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4"
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
     ]
 )
