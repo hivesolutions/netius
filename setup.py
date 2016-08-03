9cc0224317ac8f995888241b9d7027d214317731
joamag <joamag@hive.pt>
2016-08-03 16:49:13 +0100

    new version

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ import netius.common
 netius.common.ensure_setup()
 setuptools.setup(
     name = "netius",
-    version = "1.8.25",
+    version = "1.8.26",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
--- src/netius/base/common.py
+++ src/netius/base/common.py
@@ -62,7 +62,7 @@ NAME = "netius"
 identification of both the clients and the services this
 value may be prefixed or suffixed """
 
-VERSION = "1.8.25"
+VERSION = "1.8.26"
 """ The version value that identifies the version of the
 current infra-structure, all of the services and clients
 may share this value """
