6928e0ff1131d3635eebadad2d9b36f4365abd29
joamag <joamag@gmail.com>
2016-08-05 09:34:07 +0100

    fixed issue with rouding of units

--- src/netius/common/util.py
+++ src/netius/common/util.py
@@ -293,6 +293,11 @@ def size_round_unit(
         # required to avoid strange results for depth zero
         reduce = reduce or depth == 0
 
+        # in case the dot value is not present in the size value
+        # string adds it to the end otherwise an issue may occur
+        # while removing extra padding characters
+        if not "." in size_value_s: size_value_s += "."
+
         # strips the value from zero appended to the right and
         # then strips the value also from a possible decimal
         # point value that may be included in it, this is only
