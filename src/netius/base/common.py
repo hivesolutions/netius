7fe1df41e9429a95de9bc10e79b9a206eded21a2
joamag <joamag@gmail.com>
2016-09-09 18:37:18 +0100

    new version

--- src/netius/base/common.py
+++ src/netius/base/common.py
@@ -62,7 +62,7 @@ NAME = "netius"
 identification of both the clients and the services this
 value may be prefixed or suffixed """
 
-VERSION = "1.9.33"
+VERSION = "1.9.34"
 """ The version value that identifies the version of the
 current infra-structure, all of the services and clients
 may share this value """
