251e2d34aebc8351674efea44c5dfc0d5316c281
joamag <joamag@gmail.com>
2016-10-14 21:57:13 +0100

    removed destroy call (not required)

--- src/netius/clients/smtp.py
+++ src/netius/clients/smtp.py
@@ -108,7 +108,6 @@ class SMTPConnection(netius.Connection):
     def close(self, *args, **kwargs):
         netius.Connection.close(self, *args, **kwargs)
         if self.parser: self.parser.destroy()
-        self.destroy()
 
     def build(self):
         """
