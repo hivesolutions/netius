9898050aff61483603ff4c39733de95175c225ba
joamag <joamag@gmail.com>
2016-11-06 11:24:28 +0000

    better configuration

--- src/netius/base/__init__.py
+++ src/netius/base/__init__.py
@@ -57,7 +57,7 @@ from .common import NAME, VERSION, IDENTIFIER_SHORT, IDENTIFIER_LONG,\
     IDENTIFIER, TCP_TYPE, UDP_TYPE, SSL_KEY_PATH, SSL_CER_PATH, SSL_CA_PATH,\
     SSL_DH_PATH, Base, BaseThread, get_main, get_loop,\
     get_poll, ensure, ensure_pool
-from .config import conf, conf_prefix, conf_s
+from .config import conf, conf_prefix, conf_suffix, conf_s, conf_d
 from .conn import OPEN, CLOSED, PENDING, CHUNK_SIZE, Connection
 from .container import Container, ContainerServer
 from .errors import NetiusError, PauseError, DataError, ParserError,\
--- src/netius/extra/proxy_d.py
+++ src/netius/extra/proxy_d.py
@@ -19,6 +19,9 @@
 # You should have received a copy of the Apache License along with
 # Hive Netius System. If not, see <http://www.apache.org/licenses/>.
 
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
 __version__ = "1.0.0"
 """ The version of the module """
 
@@ -34,23 +37,13 @@ __copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
 __license__ = "Apache License, Version 2.0"
 """ The license for the module """
 
-from . import desktop
-from . import dhcp_s
-from . import file
-from . import filea
-from . import hello_w
-from . import hello
-from . import proxy_d
-from . import proxy_f
-from . import proxy_r
-from . import smtp_r
+import netius
 
-from .desktop import DesktopServer
-from .dhcp_s import DHCPServerS
-from .file import FileServer
-from .filea import FileAsyncServer
-from .hello import HelloServer
-from .proxy_d import DockerProxyServer
-from .proxy_f import ForwardProxyServer
-from .proxy_r import ReverseProxyServer
-from .smtp_r import RelaySMTPServer
+from . import proxy_r
+
+class DockerProxyServer(proxy_r.ReverseProxyServer):
+
+    def on_serve(self):
+        proxy_r.ReverseProxyServer.on_serve(self)
+        ports = netius.conf_suffix("_PORT")
+        print(ports)
