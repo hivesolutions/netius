27eeb30346654b514a86f8218635cac40695b98b
joamag <joamag@gmail.com>
2016-11-15 07:15:55 +0000

    intial support for alias

--- src/netius/extra/proxy_d.py
+++ src/netius/extra/proxy_d.py
@@ -61,20 +61,51 @@ class DockerProxyServer(proxy_r.ReverseProxyServer):
         self._build_hosts()
         self._build_alias()
 
-    def _build_hosts(self):
+    def _build_hosts(self, alias = True):
+        # tries to retrieve the complete set of configuration
+        # values associated with the port suffix, this represents
+        # the possible linked container addresses
         linked = netius.conf_suffix("_PORT")
+
+        # iterates over the linked values, validating them and adding
+        # them to the list of registered hosts
         for name, host in netius.legacy.iteritems(linked):
+            # retrieves the name part of the configuration name
+            # and converts it into lower cased value, note that
+            # an extra dashed version is created, so that a proper
+            # alias may be created for such naming
             base = name[:-5].lower()
             base_dash = base.replace("_", "-")
+
+            # "builds" the name reference of the service and tries
+            # to retrieve it from the configuration, it should exist
+            # in case this port value represent a service
             name_ref = base.upper() + "_NAME"
             name_value = netius.conf(name_ref, None)
+
+            # runs a series of validation on both the base and name
+            # value to make sure that this value represents a valid
+            # linked service/container
             if name.endswith("_ENV_PORT"): continue
             if base[-1].isdigit(): continue
             if not name_value: continue
+
+            # replaces the prefix of the reference (assumes HTTP) and
+            # then adds the base value to the registered hosts
             host = host.replace("tcp://", "http://")
             host = str(host)
             self.hosts[base] = host
-            self.hosts[base_dash] = host
+
+            # validates that the dashed version of the name is not the
+            # same as the base one (at least one underscore) and if that's
+            # not the case skips the current iteration
+            if base == base_dash: continue
+
+            # checks if the alias based registration is enabled and adds
+            # the dashed version as an alias for such case or as an host
+            # otherwise (static registration)
+            if alias: self.alias[base_dash] = base
+            else: self.hosts[base_dash] = host
 
     def _build_alias(self):
         linked = netius.conf_suffix("_ALIAS")
@@ -84,13 +115,16 @@ class DockerProxyServer(proxy_r.ReverseProxyServer):
             self.alias[base] = host
             self.alias[base_dash] = host
 
-    def _build_suffixes(self):
+    def _build_suffixes(self, alias = True):
         for host_suffix in self.host_suffixes:
             self.info("Registering %s host suffix" % host_suffix)
-            for host, value in netius.legacy.items(self.hosts):
-                self.hosts[host + "." + str(host_suffix)] = value
+            for name, value in netius.legacy.items(self.hosts):
+                fqn = name + "." + str(host_suffix)
+                if alias: self.alias[fqn] = name
+                else: self.hosts[fqn] = value
             for alias, value in netius.legacy.items(self.alias):
-                self.alias[alias + "." + str(host_suffix)] = value
+                fqn = alias + "." + str(host_suffix)
+                self.alias[fqn] = value
 
 if __name__ == "__main__":
     server = DockerProxyServer()
