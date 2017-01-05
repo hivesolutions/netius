0bc35a434b99f1269cf272e78d5952a607174935
joamag <joamag@hive.pt>
2017-01-05 11:21:49 +0000

    new version

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ import netius.common
 netius.common.ensure_setup()
 setuptools.setup(
     name = "netius",
-    version = "1.13.11",
+    version = "1.14.0",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
