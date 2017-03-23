eb0ea2d3a5485b43daf338890a409297d691ef9d
joamag <joamag@gmail.com>
2017-03-23 21:40:54 +0000

    small wrapper fix

--- src/netius/base/async_neo.py
+++ src/netius/base/async_neo.py
@@ -89,7 +89,7 @@ class CoroutineWrapper(object):
         return self
 
     def __next__(self):
-        if self._buffer: self._buffer.pop(0)
+        if self._buffer: return self._buffer.pop(0)
         return self.coroutine.send(None)
 
     def next(self):
