4e64af130a7cb72208830ff89ee884f4765bf03b
joamag <joamag@gmail.com>
2017-04-09 14:10:02 +0100

    new parse values

--- src/netius/clients/dns.py
+++ src/netius/clients/dns.py
@@ -220,10 +220,16 @@ class DNSResponse(netius.Response):
         index, address = self.parse_ip4(data, index)
         return (index, address)
 
+    def parse_aaaa(self, data, index):
+        pass
+
     def parse_mx(self, data, index):
         index, preference = self.parse_short(data, index)
         index, address = self.parse_label(data, index)
         return (index, (preference, address))
+
+    def parse_cname(self, data, index):
+        pass
 
     def parse_ns(self, data, index):
         pass
