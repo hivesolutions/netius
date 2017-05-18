4f9e4c88903db21081017eea46f9e0edea099fa0
joamag <joamag@hive.pt>
2017-05-18 15:17:00 +0100

    new code

--- src/netius/base/server.py
+++ src/netius/base/server.py
@@ -905,11 +905,11 @@ class StreamServer(Server):
         #    socket.SO_RCVLOWAT,
         #    10
         #)
-        #socket_c.setsockopt(
-        #    socket.SOL_SOCKET,
-        #    socket.SO_SNDLOWAT,
-        #    10
-        #)
+        socket_c.setsockopt(
+            socket.SOL_SOCKET,
+            socket.SO_SNDLOWAT,
+            10
+        )
 
         # the process creation is considered completed and a new
         # connection is created for it and opened, from this time
