ccebc108d6d995a2465d7a7323360bd28321e95d
joamag <joamag@gmail.com>
2017-07-07 12:07:52 +0100

    new legacy values

--- src/netius/base/legacy.py
+++ src/netius/base/legacy.py
@@ -229,7 +229,7 @@ def str(value, encoding = "latin-1", errors = "strict", force = False):
     if not PYTHON_3 and not force: return value
     if value == None: return value
     if type(value) in STRINGS: return value
-    return value.decode(encoding, errors = errors)
+    return value.decode(encoding, errors)
 
 def u(value, encoding = "utf-8", errors = "strict", force = False):
     if PYTHON_3 and not force: return value
@@ -328,6 +328,12 @@ def build_opener(*args, **kwargs):
 def urlparse(*args, **kwargs):
     return _urlparse.urlparse(*args, **kwargs)
 
+def urlunparse(*args, **kwargs):
+    return _urlparse.urlunparse(*args, **kwargs)
+
+def parse_qs(*args, **kwargs):
+    return _urlparse.parse_qs(*args, **kwargs)
+
 def urlencode(*args, **kwargs):
     if PYTHON_3: return urllib.parse.urlencode(*args, **kwargs)
     else: return urllib.urlencode(*args, **kwargs) #@UndefinedVariable
@@ -347,10 +353,6 @@ def unquote(*args, **kwargs):
 def unquote_plus(*args, **kwargs):
     if PYTHON_3: return urllib.parse.unquote_plus(*args, **kwargs)
     else: return urllib.unquote_plus(*args, **kwargs) #@UndefinedVariable
-
-def parse_qs(*args, **kwargs):
-    if PYTHON_3: return urllib.parse.parse_qs(*args, **kwargs)
-    else: return _urlparse.parse_qs(*args, **kwargs) #@UndefinedVariable
 
 def cmp_to_key(*args, **kwargs):
     if PYTHON_3: return dict(key = functools.cmp_to_key(*args, **kwargs)) #@UndefinedVariable
