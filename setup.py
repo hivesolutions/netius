d21a25cd4166fe4574bac94849933164cee5888d
joamag <joamag@hive.pt>
2018-04-18 19:19:36 +0100

    new version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def read_file(path):
 netius.common.ensure_setup()
 setuptools.setup(
     name = "netius",
-    version = "1.17.21",
+    version = "1.17.22",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
