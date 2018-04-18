eb7f24cc094a670f8cae7a419fbd2096d979156d
joamag <joamag@hive.pt>
2018-04-18 10:21:20 +0100

    included fix again

--- src/netius/clients/http.py
+++ src/netius/clients/http.py
@@ -680,7 +680,8 @@ class HTTPProtocol(netius.StreamProtocol):
             # receive operations is valid or there's data still pending
             # to be sent to the server side, and if that's the case delays
             # the timeout verification according to the timeout value
-            if not self.is_open() or delta < self.timeout:
+            if not self.is_open() or delta < self.timeout or\
+                not self.transport().get_write_buffer_size() == 0:
                 self.delay(receive_timeout, timeout = self.timeout)
                 return
 
