b4853fcd6d31b76f25a33ee04f88f7238e21a917
joamag <joamag@gmail.com>
2019-09-09 16:39:30 +0100

    New version 1.16.50

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def read_file(path):
 netius.common.ensure_setup()
 setuptools.setup(
     name = "netius",
-    version = "1.16.49",
+    version = "1.16.50",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
