ac2ae7a4e3064c5caec5f1556bce7b6c0b7f6bc1
joamag <joamag@gmail.com>
2019-09-17 16:28:12 +0100

    New support for config of nameserver

--- src/netius/clients/dns.py
+++ src/netius/clients/dns.py
@@ -336,11 +336,21 @@ class DNSClient(netius.DatagramClient):
 
     @classmethod
     def ns_system(cls, type = "ip4"):
+        ns = cls.ns_conf(type = type)
+        if ns: return ns[0]
         ns = cls.ns_file(type = type)
         if ns: return ns[0]
         ns = cls.ns_google(type = type)
         if ns: return ns[0]
         return None
+
+    @classmethod
+    def ns_conf(cls, type = "ip4", force = False):
+        ns =  netius.conf("NAMESERVER_%s" % type.upper(), [], cast = list)
+        if ns: return ns
+        ns = netius.conf("NAMESERVER", [], cast = list)
+        if ns: return ns
+        return []
 
     @classmethod
     def ns_file(cls, type = "ip4", force = False):
