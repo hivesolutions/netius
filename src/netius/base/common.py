f2336f488ff5b2fd9599415c234d0b5fea093813
João Magalhães <joamag@gmail.com>
2021-02-02 13:04:35 +0000

    version: 1.18.0

--- src/netius/base/common.py
+++ src/netius/base/common.py
@@ -66,7 +66,7 @@ NAME = "netius"
 identification of both the clients and the services this
 value may be prefixed or suffixed """
 
-VERSION = "1.17.60"
+VERSION = "1.18.0"
 """ The version value that identifies the version of the
 current infra-structure, all of the services and clients
 may share this value """
