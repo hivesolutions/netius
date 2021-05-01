93d817ba12e7931365b714c42be39c337be1370f
João Magalhães <joamag@gmail.com>
2021-05-01 11:07:17 +0100

    version: 1.18.2

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def read_file(path):
 netius.common.ensure_setup()
 setuptools.setup(
     name = "netius",
-    version = "1.18.1",
+    version = "1.18.2",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
