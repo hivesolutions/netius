5173defc231c7f7ffe15fa7d91a699805ecc768f
João Magalhães <joamag@gmail.com>
2021-08-22 18:32:38 +0100

    docs: small doc fix

--- src/netius/base/service.py
+++ src/netius/base/service.py
@@ -46,8 +46,8 @@ class Service(observer.Observable):
     Top level class responsible for the single representation
     of the meta-data associated with a service.
 
-    This is considered to be the equivalent to a connection object
-    for the servers (as opposed clients).
+    This is considered to be the equivalent to a stream object
+    for the servers (as opposed to clients).
     """
 
     def __init__(
