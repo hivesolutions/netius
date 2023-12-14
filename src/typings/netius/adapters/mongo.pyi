5873ca1cb7bdfce349cac7ba38734af5ba75054b
Hugo Gomes <hugo@hugogomes.eu>
2023-12-14 19:03:42 +0000

    review src/typings/netius/adapters/mongo.pyi

--- src/typings/netius/adapters/mongo.pyi
+++ src/typings/netius/adapters/mongo.pyi
@@ -1,10 +1,6 @@
 import netius.adapters.base
 import netius.adapters.base as base
 
-__version__: str
-__revision__: str
-__date__: str
-
 class MongoAdapter(netius.adapters.base.BaseAdapter):
     def set(self, value, owner: str = ...): ...
     def get(self, key): ...
