0ccbf1d7b9dd2cccbb1bd0020d7f17623cbb0cfa
Hugo Gomes <hugo@hugogomes.eu>
2023-12-14 19:02:44 +0000

    review src/typings/netius/adapters/__init__.pyi

--- src/typings/netius/adapters/__init__.pyi
+++ src/typings/netius/adapters/__init__.pyi
@@ -4,7 +4,3 @@ from netius.adapters.fs import FsAdapter as FsAdapter
 from netius.adapters.memory import MemoryAdapter as MemoryAdapter
 from netius.adapters.mongo import MongoAdapter as MongoAdapter
 from netius.adapters.null import NullAdapter as NullAdapter
-
-__version__: str
-__revision__: str
-__date__: str
