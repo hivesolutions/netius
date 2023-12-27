6740de227b412aec1504982f0187cd56546c0247
João Magalhães <joamag@gmail.com>
2023-12-27 15:18:54 +0000

    chore: more simplification

--- src/netius/auth/allow.pyi
+++ src/netius/auth/allow.pyi
@@ -1,10 +1,4 @@
-import netius.auth.base
-import netius.auth.base as base
+from netius import Auth
 
-from typing import Literal
-
-class AllowAuth(netius.auth.base.Auth):
-    @classmethod
-    def auth(cls, *args, **kwargs) -> Literal[True]: ...
-    @classmethod
-    def is_simple(cls) -> Literal[True]: ...
+class AllowAuth(Auth):
+    pass
