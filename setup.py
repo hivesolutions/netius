c5777e31668cc7ee44a8e38ea76061a19f4deb7f
João Magalhães <joamag@gmail.com>
2023-12-27 23:01:12 +0000

    chore: new python version compatibility

--- setup.py
+++ setup.py
@@ -105,7 +105,12 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
     ],
     long_description = read_file("README.rst")
 )
