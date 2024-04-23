84b31c68bb2c7d140f03f670526578b7700b8fbd
João Magalhães <joamag@gmail.com>
2024-04-23 14:00:33 +0100

    chore: raise_e optional support in LogstashHandler

--- src/netius/base/log.py
+++ src/netius/base/log.py
@@ -92,7 +92,7 @@ class LogstashHandler(logging.Handler):
             return False
         return True
 
-    def emit(self, record):
+    def emit(self, record, raise_e=False):
         from . import common
 
         # verifies if the API structure is defined and set and if
@@ -145,9 +145,12 @@ class LogstashHandler(logging.Handler):
         should_flush = len(self.messages) >= self.max_length
         should_flush = should_flush or time.time() - self._last_flush > self.timeout
         if should_flush:
-            self.flush()
+            try:
+                self.flush(raise_e=raise_e)
+            except Exception:
+                if raise_e: raise
 
-    def flush(self, force=False):
+    def flush(self, force=False, raise_e=False):
         logging.Handler.flush(self)
 
         # verifies if the API structure is defined and set and if
@@ -163,7 +166,7 @@ class LogstashHandler(logging.Handler):
 
         # posts the complete set of messages to logstash and then clears the messages
         # and updates the last flush time
-        self.api.log_bulk(messages, tag="default")
+        self.api.log_bulk(messages, tag="default", raise_e=raise_e)
         self.messages = []
         self.last_flush = time.time()
 
