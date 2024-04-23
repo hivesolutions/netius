57f4baed24f301be90242d09c6b8ae76cde092ad
João Magalhães <joamag@gmail.com>
2024-04-23 22:15:41 +0100

    version: 1.19.12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -19,6 +19,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 *
 
+## [1.19.12] - 2024-04-23
+
+### Changed
+
+* Much richer logging context support
+
 ## [1.19.11] - 2024-04-23
 
 ### Fixed
--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def read_file(path):
 netius.common.ensure_setup()
 setuptools.setup(
     name = "netius",
-    version = "1.19.11",
+    version = "1.19.12",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Netius System",
