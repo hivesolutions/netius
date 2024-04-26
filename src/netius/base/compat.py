54cf62b674c3c030ce2c67defe305212c51324f6
João Magalhães <joamag@gmail.com>
2024-04-26 17:29:19 +0100

    chore: removed extra todo

--- src/netius/base/compat.py
+++ src/netius/base/compat.py
@@ -823,9 +823,6 @@ def _serve_stream_compat(
     :see: https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.create_server
     """
 
-    # @TODO: implement this stuff meaning that the compat
-    # mode is the mode in which Netius runs compatible
-    # with the asyncio module
     from . import common
 
     loop = loop or common.get_loop()
