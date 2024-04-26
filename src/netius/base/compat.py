852a63650851b152cfe0cf0c8e7c9331df172bee
João Magalhães <joamag@gmail.com>
2024-04-26 12:22:20 +0100

    docs: new documentation references

--- src/netius/base/compat.py
+++ src/netius/base/compat.py
@@ -330,6 +330,8 @@ class CompatLoop(BaseLoop):
         ssl_handshake_timeout=None,
     ):
         # @TODO: this is pending proper Netius implementation
+        # to make it compatible with
+        # https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.Server.start_serving
         self._add_reader(
             sock.fileno(),
             self._accept_connection,
