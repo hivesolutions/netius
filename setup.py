21d39f5db0609b58fb631ce0babd696227424694
joamag <joamag@gmail.com>
2026-04-02 12:20:08 +0100

    chore: added python 3.14 version

--- setup.py
+++ setup.py
@@ -108,6 +108,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     long_description=read_file("README.rst"),
 )
