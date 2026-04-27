8adfdd5f5cbf3b27dad372ab8179f9dbcd3b7aa1
João Magalhães <joamag@gmail.com>
2026-04-27 20:09:33 +0100

    version: 1.53.12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+*
+
+## [1.53.12] - 2026-04-27
+
+### Fixed
+
 * Version bumping number
 
 ## [1.53.11] - 2026-04-27
--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ if not os.environ.get("SKIP_SETUP", "0") == "1":
 
 setuptools.setup(
     name="netius",
-    version="1.53.11",
+    version="1.53.12",
     author="Hive Solutions Lda.",
     author_email="development@hive.pt",
     description="Netius System",
